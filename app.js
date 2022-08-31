a0d1b01e23aeddf1f871c72d3298c39b9b213fe4
sivasubramanian11 <Sivaifs4@gmail.com>
2022-08-31 18:16:29 +0530

    Selected input elements from html to JS

---
+++ app.js
@@ -0,0 +1,7 @@
+//step-1 Selecting all the elements from Html input
+var initialPrice = document.querySelector('#initial-price');
+var stocksQuantity = document.querySelector('#stocks-quantity');
+var currentPrice = document.querySelector('#current-price');
+var submitBtn = document.querySelector('#submit-btn');
+var outputBox = document.querySelector('#output-box');
+
