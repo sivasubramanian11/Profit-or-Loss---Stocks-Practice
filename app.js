8baa08902cfdf1a6ae64946d77beeea51aff72b0
sivasubramanian11 <Sivaifs4@gmail.com>
2022-08-31 19:23:11 +0530

    Build: Done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var outputBox = document.querySelector('#output-box');
 
 submitBtn.addEventListener('click', submitHandler);
 
-function submitHandler(){
+function submitHandler() {
     var ip = initialPrice.value;
     var qty = stocksQuantity.value;
     var curr = currentPrice.value;
@@ -16,20 +16,17 @@ function submitHandler(){
 }
 
 //Logic building done
-function CalculateProfitAndLoss(initial, quantity, current){
-    if(initial > current){
+function CalculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
         var loss = (initial - current) * quantity;
         var lossPercentage = (loss / initial) * 100;
-        
-       showOutput(`The loss is ${loss} and the percentage is ${lossPercentage}%`);
-    }
-    else if(current > initial){
-       var profit = (current - initial) * quantity;
-       var profitPercentage = (profit / initial) * 100;
-      showOutput(`The Profit is ${profit} and the profit percentage is ${profitPercentage}`);
-    }
-    else{
-       showOutput("No pain No gain!!");
+        showOutput(`The loss is ${loss} and the percentage is ${lossPercentage}%`);
+    } else if (current > initial) {
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
+        showOutput(`The Profit is ${profit} and the profit percentage is ${profitPercentage}`);
+    } else {
+        showOutput("No pain No gain!!");
     }
 }
 
