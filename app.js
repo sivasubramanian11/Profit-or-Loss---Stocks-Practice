b649dcf74417fdd060c9b3e85d6bbf54e8458da5
sivasubramanian11 <Sivaifs4@gmail.com>
2022-08-31 18:46:50 +0530

    Logic Done

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ var currentPrice = document.querySelector('#current-price');
 var submitBtn = document.querySelector('#submit-btn');
 var outputBox = document.querySelector('#output-box');
 
+function CalculateProfitAndLoss(initial, quantity, current){
+    if(initial > current){
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+        
+        console.log(`The loss is ${loss} and the percentage is ${lossPercentage}%`);
+    }
+    else if(current > initial){
+       var profit = (current - initial) * quantity;
+       var profitPercentage = (profit / initial) * 100;
+       console.log(`The Profit is ${profit} and the profit percentage is ${profitPercentage}`);
+    }
+    else{
+        console.log("No pain No gain!!");
+    }
+}
+
+CalculateProfitAndLoss(100, 10, 100 );
